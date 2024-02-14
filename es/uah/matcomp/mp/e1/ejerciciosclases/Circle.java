d79775f6b8a76e6ba08b4fa0bdb74493261e7dfa
= <=>
2024-02-14 13:34:11 +0100

    Guardar

---
+++ es/uah/matcomp/mp/e1/ejerciciosclases/Circle.java
@@ -0,0 +1,29 @@
+package es.uah.matcomp.mp.e1.ejerciciosclases;
+
+public class Circle {
+    private double radius;
+    public Circle(double r){
+        radius=r;
+    }
+    public Circle(){
+        radius = 1.0;
+    }
+    public double getRadius(){
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public double getArea(){
+        return radius*radius*Math.PI;
+    }
+    public double getCircumference(){
+        return 2*radius*Math.PI;
+    }
+    public String toString(){
+        return "Circle[radius="+ radius + "]";
+    }
+}
+
+
