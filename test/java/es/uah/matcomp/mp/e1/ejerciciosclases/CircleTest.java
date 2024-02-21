0d8a9375f671dc1f0538720d02b978b5b3c8ab36
= <=>
2024-02-21 20:20:39 +0100

    Tests Circle y Rectangle

--- test/java/es/uah/matcomp/mp/e1/ejerciciosclases/CircleTest.java
+++ test/java/es/uah/matcomp/mp/e1/ejerciciosclases/CircleTest.java
@@ -1,29 +1,53 @@
 package es.uah.matcomp.mp.e1.ejerciciosclases;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CircleTest {
-
     @Test
-    void getRadius() {
-
+    public void getRadius() {
+        double radioEsperado = 1.0;
+        Circle nuevoCirculo = new Circle(radioEsperado);
+        double radioObtenido = nuevoCirculo.getRadius();
+        Assertions.assertEquals(radioEsperado,radioObtenido);
     }
 
     @Test
     void setRadius() {
+        double radioEsperado = 1.0;
+        double radioFinal = 5.0;
+        Circle nuevoCirculo = new Circle(radioFinal);
+        nuevoCirculo.setRadius(radioEsperado);
+        double radioObtenido = nuevoCirculo.getRadius(); ;
+        Assertions.assertEquals(radioEsperado,radioObtenido);
     }
-
     @Test
     void getArea() {
+        double areaEsperada = 4*Math.PI;
+        double radio = 2.0;
+        Circle nuevoCirculo = new Circle(radio);
+        double areaObtenida = nuevoCirculo.getArea();
+        Assertions.assertEquals(areaEsperada, areaObtenida);
     }
 
     @Test
     void getCircumference() {
+        double perimetroEsperado = 4*Math.PI;
+        double radio = 2.0;
+        Circle nuevoCirculo = new Circle(radio);
+        double perimetroObtenido = nuevoCirculo.getCircumference();
+        Assertions.assertEquals(perimetroEsperado, perimetroObtenido);
     }
 
     @Test
     void testToString() {
+        String mensajeEsperado = "Circle[radius=5.0]";
+        double radius = 5.0;
+        Circle nuevoCirculo = new Circle(radius);
+        String mensajeObtenido = nuevoCirculo.toString();
+        Assertions.assertEquals(mensajeEsperado, mensajeObtenido);
     }
+
 }
--- test/java/es/uah/matcomp/mp/e1/ejerciciosclases/RectangleTest.java
+++ test/java/es/uah/matcomp/mp/e1/ejerciciosclases/RectangleTest.java
@@ -1,5 +1,6 @@
 package es.uah.matcomp.mp.e1.ejerciciosclases;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,29 +9,71 @@ class RectangleTest {
 
     @Test
     void getLength() {
+        float lengthEsperado = 33f;
+        float widthEsperado = 33f;
+        Rectangle nuevoRectangulo = new Rectangle(lengthEsperado,widthEsperado);
+        float lengthObtenido = nuevoRectangulo.getLength();
+        Assertions.assertEquals(lengthEsperado,lengthObtenido);
     }
 
     @Test
     void setLength() {
+        float lengthEsperado = 33f;
+        float widthEsperado = 33f;
+        float newLength = 43f;
+        Rectangle nuevoRectangulo = new Rectangle(newLength,widthEsperado);
+        nuevoRectangulo.setLength(lengthEsperado);
+        float lengthObtenido = nuevoRectangulo.getLength();
+        Assertions.assertEquals(lengthEsperado,lengthObtenido);
     }
 
     @Test
     void getWidth() {
+        float lengthEsperado = 33f;
+        float widthEsperado = 33f;
+        Rectangle nuevoRectangulo = new Rectangle(lengthEsperado,widthEsperado);
+        float widthObtenido = nuevoRectangulo.getWidth();
+        Assertions.assertEquals(widthEsperado,widthObtenido);
     }
 
     @Test
     void setWidth() {
+        float lengthEsperado = 33f;
+        float widthEsperado = 33f;
+        float newwidth = 43f;
+        Rectangle nuevoRectangulo = new Rectangle(lengthEsperado,newwidth);
+        nuevoRectangulo.setLength(lengthEsperado);
+        float lengthObtenido = nuevoRectangulo.getWidth();
+        Assertions.assertEquals(lengthEsperado,lengthObtenido);
     }
 
     @Test
     void getArea() {
+        float length = 33f;
+        float width = 33f;
+        float areaEsperada = (float) Math.pow(33f, 2);
+        Rectangle nuevoRectangulo = new Rectangle(length, width);
+        double areaObtenida = nuevoRectangulo.getArea();
+        Assertions.assertEquals(areaEsperada,areaObtenida);
     }
 
     @Test
     void getPerimeter() {
+        float length = 33f;
+        float width = 33f;
+        float perimetroEsperado = 132 ;
+        Rectangle nuevoRectangulo = new Rectangle(length, width);
+        double perimetroObtenido = nuevoRectangulo.getPerimeter(); //el tipo de dato tiene que ser igual que el que se ha definido en el metodo
+        Assertions.assertEquals(perimetroEsperado,perimetroObtenido);
     }
 
     @Test
     void testToString() {
+        float length = 33.3f;
+        float width = 33.3f;
+        String mensajeEsperado = "Rectangle [lenght=33.3, width=33.3]";
+        Rectangle nuevoRectangulo = new Rectangle(length, width);
+        String mensajeObtenido = nuevoRectangulo.toString();
+        Assertions.assertEquals(mensajeEsperado,mensajeObtenido);
     }
 }
