32d2cbba7b2ef3480a92c7bb0b7befaae8b6c97e
= <=>
2024-02-28 12:49:39 +0100

    Commit

---
+++ es/uah/matcomp/mp/e1/ejerciciosclases/Book.java
@@ -0,0 +1,52 @@
+package es.uah.matcomp.mp.e1.ejerciciosclases;
+
+public class Book {
+    private String isbn;
+    private String name;
+    private Author author;
+    private double price;
+    private int qty = 0;
+    public Book(String isbn, String name, Author author, double price){
+        this.isbn = isbn;
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        qty = 0;
+    }
+    public Book(String isbn, String name, Author author, double price, int qty){
+        this.isbn = isbn;
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.qty = qty;
+    }
+    public String getIsbn() {
+        return isbn;
+    }
+    public String getName() {
+        return name;
+    }
+    public Author getAuthor() {
+        return author;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public int getQty() {
+        return qty;
+    }
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+    public String getAuthorName(){
+        author.getName();
+        author.getEmail();
+        String authorName =
+    }
+    public String toString(){
+        return "Book[isbn="+isbn+","+author.toString()+",price="+price+",qty="+qty+"]";
+    }
+}
